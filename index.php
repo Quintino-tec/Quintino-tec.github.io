57b382eb2152fd18e30800fcb99ff9d246ddeeb7
Quintino-tec <filhoquintino4@gmail.com>
2023-03-02 09:39:14 -0300

    arrumando a index php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página 2</title>
+</head>
+<body>
 <?php
 $_v1 = 1;
 $_v2 =2;
@@ -8,3 +17,5 @@ if ($_v1 > $_v2) {
     echo "Deu falha";
 }
 ?>
+</body>
+</html>
